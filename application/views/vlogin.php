c2e3e59449c70480a84305cba4be6eabe22b3d41
sharkcreep87 <shafeq.hasan@gmail.com>
2014-11-12 09:31:10 +0800

    change UI

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -1,67 +1,64 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    
-    <link rel="icon" href="<?php echo base_url();?>images/Lock1.ico">
-    <title>Log In</title>
-	<link href="<?php echo base_url();?>assets/plugins/bootstrap/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.css" rel="stylesheet">
-	<link href="<?php echo base_url();?>assets/plugins/pace/pace-theme-big-counter.css" rel="stylesheet">
-	<link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
-	<link href="<?php echo base_url();?>assets/css/main-style.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <link rel="shortcut icon" href="img/favicon.png">
 
-	
+    <title>SyloBpsm</title>
 
-  </head>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
 
-  <body>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
 
-
+  <body class="lock-screen">
 
     <div class="container">
-       
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 text-center logo-margin ">
-              <img src="<?php echo base_url();?>assets/img/logo.jpg" alt=""/>
-                </div>
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">                  
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Log Masuk</h3>
-                    </div>
-                    <div class="panel-body">
-                         <form class="form-signin" role="form"  method="post" action="<?php  $this->load->helper('url'); echo site_url("/login/process"); ?>">
-        
-        
-		<?php if(! is_null($msg)) echo $msg;?>  
-		
-                                          <div class="form-group">
-                                        <input class="form-control" type="text" class="form-control" id ="UID" name ="UID" placeholder="Username" required autofocus>                                        
-                                </div>
-									
-								
-                                          <div class="form-group">
-                                        <input type="password" class="form-control" id ="Password" name ="Password" placeholder="Password" required>
-                                </div>
-								   <div class="form-group">
-		<select class="form-control" id= "User_type" name="User_type">
-			<option value="User">Kakitangan</option>
-			<option value="Admin">Admin</option>			
-		</select></div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Log Masuk</button>
-		<hr></hr>
-     <center> <p> <a href="<?php  $this->load->helper('url'); echo site_url("/creg"); ?>" >Daftar</a> untuk penguna baru </p></center>
-	  </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-   
 
-     <!-- Core Scripts - Include with every page -->
-    <script src="<?php echo base_url();?>assets/plugins/jquery-1.10.2.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/bootstrap/bootstrap.min.js"></script>
-    <script src="<?php echo base_url();?>assets/plugins/metisMenu/jquery.metisMenu.js"></script>
+       <form class="form-signin" role="form"  method="post" action="<?php  $this->load->helper('url'); echo site_url("/login/process"); ?>">
+        <h2 class="form-signin-heading">Log Masuk</h2>
+        <div class="login-wrap">
+            <input class="form-control" type="text" class="form-control" id ="UID" name ="UID" placeholder="Username" required autofocus> 
+           <input type="password" class="form-control" id ="Password" name ="Password" placeholder="Password" required>
+           
+            <button class="btn btn-lg btn-login btn-block" type="submit">Log Masuk</button>
+           
+           
+            <div class="registration">
+                Daftar Akaun Baru?
+                <a class="" href="<?php  $this->load->helper('url'); echo site_url("/creg"); ?>">
+                    Daftar
+                </a>
+            </div>
+
+        </div>
+
+
+      </form>
+
+    </div>
+
+
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
   </body>
 </html>
--- application/views/vreg.php
+++ application/views/vreg.php
@@ -1,89 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    
-   
-    <link rel="icon" href="<?php echo base_url();?>images/Lock1.ico">
-    <title>Registration</title>
-    <link href="<?php echo base_url();?>css/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>css/css/bootstrap.min.css" rel="stylesheet">
-<style>
+<head>
+  
+    <title>SyloBpsm</title>
 
-body {
-  
-  background-color: #eee;
-  
-}
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url();?>css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>css/style-responsive.css" rel="stylesheet" />
 
-.form-horizontal {
-  max-width: 630px;
-  padding: 25px;
-  margin: 0 auto;
-}
-.form-control {
- 
-  margin-bottom: 10px;
-}
-  
-.navbar-collapse {
-        position: absolute;
-        left: 40%;
-        text-align: center;
-     top:15%;
-	  font-family: 'Georgia';
-    }
-	.navbar-brand-centered1 {
-        position: absolute;
-        left: 22%;
-		text-align: center;
-		top:-20%;
-		 font-family: 'Arial';
-    }
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
 
-</style>
-	
-	<script src="<?php echo base_url();?>css/js/jquery.min.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap.js"></script>
-  </head>
+  <body class="lock-screen">
 
-  <body>
-  
- <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="collapse navbar-collapse">
-       
-     <h4>Please fill out the form</h4>
-        
-    </div>
-	<div class="navbar-brand navbar-brand-centered1">
-       
-     <h4><a href="<?php  $this->load->helper('url'); echo site_url("login"); ?>">
-			  <span class="glyphicon glyphicon-circle-arrow-left"></span> BACK</a></h4>  
-    
-        
-    </div>
-    </div>  
-	
-   <div class="jumbotron">
- 
-   <div class="container">
-    <div class="row">
-   
-		<div class="col-md-6 col-md-offset-3">       
-  
-	  <div class="panel panel-default">
-	    
-	  <div class="panel-heading">Registration</div>
-              <form class="form-horizontal" method="post" action="<?php  $this->load->helper('url'); echo site_url("/creg/insert"); ?>">
+    <div class="container">
+
+       <form class="form-horizontal" method="post" action="<?php  $this->load->helper('url'); echo site_url("/creg/insert"); ?>">
 			
 			
 			 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $this->load->helper('form');
 ?>
-			  <fieldset>
+			  <div class="login-wrap">
 			<label for="inputName">Full Name</label>
+			
 			<input type="text" class="form-control" id ="Name" name = "Name" placeholder="Full Name"  value="<?php echo set_value('Name'); ?>" required autofocus>
 			<label for="inputID">Identity Card No</label>
 			<input type="text" class="form-control" name = "ID" placeholder="Identity Card No" value="<?php echo set_value('ID'); ?>" required>
@@ -100,14 +50,19 @@ $this->load->helper('form');
 			<input type="password" class="form-control" name = "Password" placeholder="Password" required>
 			<label for="inputEmail">Email</label>
             <input type="email" class="form-control" name = "Email" placeholder="Email" value="<?php echo set_value('Email'); ?>" required>
-  <button type="submit" class="btn btn-primary btn-block" >Register</button>
+
+ <button type="submit" class="btn btn-primary btn-block" >Register</button>
   <button class="btn btn-default btn-block" type="reset">Reset</button>
-</fieldset></form>
-         
-      </div>
-	  </div>
-	 </div>
-      </div>
-</div>
+</div></form>
+
+    </div>
+
+
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url();?>js/jquery.js"></script>
+    <script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+
+
   </body>
 </html>
