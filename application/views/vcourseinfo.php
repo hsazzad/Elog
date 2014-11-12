0f3b6fca9749506838a0f500d65e173cfc13243f
sharkcreep87 <shafeq.hasan@gmail.com>
2014-11-12 15:40:05 +0800

    add ui for register course

--- application/views/vcourseinfo.php
+++ application/views/vcourseinfo.php
@@ -1,85 +1,5 @@
-<?php 
- 
-header("cache-Control: no-store, no-cache, must-revalidate");
-header("cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+<?php include 'header.php';?>
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    
-    <link rel="icon" href="<?php echo base_url();?>images/Lock1.ico">
-    <title>Course Information</title>
-	<link href="<?php echo base_url();?>css/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>css/css/bootstrap.min.css" rel="stylesheet">
-<style>
-
-body {
- 
-  background-color: #eee;
-}
-
-
-.form-horizontal {
-
-  max-width: 630px;
-  padding: 25px;
-  margin: 0 auto;
-   background-color: #fff;
-}
-.form-control {
- 
-  margin-bottom: 10px;
-}
-  
-.navbar-collapse {
-        position: absolute;
-        left: 40%;
-        text-align: center;
-     top:15%;
-	  font-family: 'Georgia';
-    }
-	.navbar-brand-centered1 {
-        position: absolute;
-        left: 70%;
-		text-align: center;
-		top:0%;
-		 font-family: 'Arial';
-    }
-</style>
-	
- <script src="<?php echo base_url();?>css/js/jquery.min.js"></script>
-    <script src="<?php echo base_url();?>css/js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap.js"></script>
-	<script src="<?php echo base_url();?>css/js/bootstrap-dropdown.js"></script>
-  </head>
-
-  
-
-  <body >
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-   
-   <ul class="nav navbar-nav">
-        <li><a href="<?php  $this->load->helper('url'); echo site_url("/home/index"); ?>">Home</a></li>
-   </ul>
-  <div class="collapse navbar-collapse">
-      <h4> You have signed in as <b> <?php echo $this->session->userdata('UID'); ?> </b> </h4>
-        
-    </div>
-	
-	<div class="navbar-brand navbar-brand-centered1">
-       
-    <b><a href="<?php  $this->load->helper('url'); echo site_url("/login/do_logout"); ?>"> <span class="glyphicon glyphicon-log-out"></span> LOGOUT</a></b>
-    
-        
-    </div>
-    </div> 
-
-	
-   <div class="jumbotron">
-   <div class="container">
 
   <div class="row">
   <div class="col-md-3">
@@ -183,5 +103,4 @@ foreach ($query1->result() as $row1)
 	 </div>
       </div>
 	  
-  </body>
-</html>
+ <?php include 'footer.php';?>
