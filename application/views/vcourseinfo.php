1426f3e89e660497a065bd52619376ad03891686
sharkcreep87 <shafeq.hasan@gmail.com>
2014-11-13 11:25:03 +0800

    add course category

--- application/views/vcourseinfo.php
+++ application/views/vcourseinfo.php
@@ -7,13 +7,13 @@
      
               <div class="row">
                
-                  <div class="col-md-6 col-md-offset-3">   
+                  <div class="col-md-8 col-md-offset-2">   
 		 
 	  
 	  
 	  <section class="panel">
                           <header class="panel-heading">
-                         <center><b>    Course Information<b></center>
+                         <center><b>Maklumat Program Latihan<b></center>
                           </header>
                           <div class="panel-body">
 	  
@@ -27,24 +27,55 @@
 			  
 			 <?php $UID=$this->session->userdata('UID');echo "<input type='hidden' id='UID' name='UID' value='$UID'>"; ?>
 		 <div class="form-group">
-			<label for="inputName">Course Name</label>
-			<input type="text" class="form-control" id ="Coursename" name = "Coursename" placeholder="Course Name" required autofocus>
+			<label for="inputName">Nama Kursus</label>
+			<input type="text" class="form-control" id ="Coursename" name = "Coursename" required autofocus>
 			 </div>
 			 <div class="form-group">
-			<label for="inputID">Place of Course</label>
-			<select class="form-control" id= "Place_type" name="Place_type">
-			<option value="Internal">Internal</option>
-			<option value="External">External</option>
-			</select>
+			
+			<label for="inputComm">Kategori Kursus</label>
+			<br/>
+			 <div class="radios">
+                                              <label class="label_radio" for="radio-01">
+                                                  <input name="course_catagory" id="course_catagory1" value="Kursus" type="radio" checked />Kursus
+                                              </label>
+                                              <label class="label_radio" for="radio-02">
+                                                  <input name="course_catagory" id="course_catagory2" value="Seminar" type="radio" />Seminar
+												  </label>
+                                              <label class="label_radio" for="radio-03">
+                                                  <input name="course_catagory" id="course_catagory3" value="Konvensyen" type="radio" /> Konvensyen
+                                              </label>
+											   <label class="label_radio" for="radio-04">
+                                                  <input name="course_catagory" id="course_catagory4" value="Bengkel/Workshop" type="radio" /> Bengkel/Workshop
+                                              </label>
+											   <label class="label_radio" for="radio-04">
+                                                  <input name="course_catagory" id="course_catagory6" value="Simposium" type="radio" /> Simposium
+                                              </label>
+											  <label class="label_radio" for="radio-04">
+                                                  <input name="course_catagory" id="course_catagory7" value="Kolokium" type="radio" /> Kolokium
+                                              </label>
+											  <label class="label_radio" for="radio-04">
+                                                  <input name="course_catagory" id="course_catagory8" value="Lawatan Rasmi" type="radio" /> Lawatan Rasmi
+                                              </label>
+											   <label class="label_radio" for="radio-04">
+                                                  <input name="course_catagory" id="course_catagory8" value="Klinik Kaunseling" type="radio" /> Klinik Kaunseling
+                                              </label>
+                                          </div>
+
+<br/>
+</div>
+			 <div class="form-group">
+			<label for="inputID">Tempat Kursus</label>
+			<input type="text" class="form-control" id ="Place_type" name = "Place_type"  required autofocus>
+			
 		</div> <div class="form-group">
-			<label for="inputGrade">Upload Certificate</label>
+			<label for="inputGrade">Muat Naik Sijil</label>
 			<input class="btn btn-file" type="file" name="Cert_file"   >
 		</div>
-			<label for="inputDate">Date of Attendance</label>
+			<label for="inputDate">Tarikh Kursus</label>
 			<input type="text" class="form-control" name = "Course_Date" placeholder="Day/Month/Year" required>
 			<div class="form-group">
 			
-			<label for="inputComm">Are you satisfied with Training/Workshop ?</label>
+			<label for="inputComm">Anda gembira mengikuti program latihan ini?</label>
 			<br/>
 			
   <input type="radio" name="Satisfaction" id="Satisfaction1" value="1"> 1
@@ -59,7 +90,7 @@
 		
 <br/>
 </div><div class="form-group">
-	<label for="inputComm">Do you acquire the knowledge that will help on your job ?</label>
+	<label for="inputComm">Anda mendapat pengetahuan yang diperlukan atau idea baru untuk menjalankan tugas.</label>
 			<br/>
 	<input type="radio" name="Knowledge" id="Knowledge1" value="1"> 1
 
@@ -74,12 +105,12 @@
 
   <input type="radio" name="Knowledge" id="Knowledge5" value="5"> 5
 <br/>
-	<label for="inputName">Comments</label>
+	<label for="inputName">Sila nyatakan penegetahuan/ idea yang mempunyai hubungkait dan bole meningkatkan mutu kerja anda :</label>
 	
     </div><div class="form-group">
     <textarea class="form-control" id="Comments" name="Comments" rows="4"></textarea>
  </div>
-  <button type="submit" class="btn btn-primary btn-block" type="submit">Submit</button>
+  <button type="submit" class="btn btn-primary btn-block" type="submit">Hantar</button>
   <button class="btn btn-default btn-block" type="reset">Reset</button>
  </fieldset>
 </form>
