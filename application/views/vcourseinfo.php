07e0db62ba58a69d862f7a8d0b6e1511966c309a
hsazzad <sazzad.utm@gmail.com>
2014-11-13 10:21:08 +0800

    courseinfo view fixed

--- application/views/vcourseinfo.php
+++ application/views/vcourseinfo.php
@@ -1,43 +1,48 @@
 <?php include 'header.php';?>
 
     
-      <!--main content start-->
-      <section id="main-content">
+    <section id="main-content">
           <section class="wrapper">
               <!-- page start-->
- 
- 	<div class="col-md-6 col-md-offset-3">      
+     
+              <div class="row">
+               
+                  <div class="col-md-6 col-md-offset-3">   
 		 
-		
-	  <section class="panel">
 	  
-	  <div class="panel-heading">Course Information</div>
+	  
+	  <section class="panel">
+                          <header class="panel-heading">
+                         <center><b>    Course Information<b></center>
+                          </header>
+                          <div class="panel-body">
+	  
 	   <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 														 $this->load->helper('form');
 															?>
-	<form  role="form" method="post" action="<?php  $this->load->helper('url'); echo site_url("courseinfo/insert"); ?>" enctype="multipart/form-data" >
+	<form role="form" method="post" action="<?php  $this->load->helper('url'); echo site_url("courseinfo/insert"); ?>" enctype="multipart/form-data" >
 	
-			
+			  <fieldset>
 			  
 			  
 			 <?php $UID=$this->session->userdata('UID');echo "<input type='hidden' id='UID' name='UID' value='$UID'>"; ?>
-		  <div class="form-group">
+		 <div class="form-group">
 			<label for="inputName">Course Name</label>
 			<input type="text" class="form-control" id ="Coursename" name = "Coursename" placeholder="Course Name" required autofocus>
-			</div>
-			<div class="form-group">
+			 </div>
+			 <div class="form-group">
 			<label for="inputID">Place of Course</label>
 			<select class="form-control" id= "Place_type" name="Place_type">
 			<option value="Internal">Internal</option>
 			<option value="External">External</option>
 			</select>
-			</div>
+		</div> <div class="form-group">
 			<label for="inputGrade">Upload Certificate</label>
 			<input class="btn btn-file" type="file" name="Cert_file"   >
-		
+		</div>
 			<label for="inputDate">Date of Attendance</label>
 			<input type="text" class="form-control" name = "Course_Date" placeholder="Day/Month/Year" required>
-			
+			<div class="form-group">
 			
 			<label for="inputComm">Are you satisfied with Training/Workshop ?</label>
 			<br/>
@@ -53,6 +58,7 @@
   <input type="radio" name="Satisfaction" id="Satisfaction5" value="5"> 5
 		
 <br/>
+</div><div class="form-group">
 	<label for="inputComm">Do you acquire the knowledge that will help on your job ?</label>
 			<br/>
 	<input type="radio" name="Knowledge" id="Knowledge1" value="1"> 1
@@ -70,13 +76,14 @@
 <br/>
 	<label for="inputName">Comments</label>
 	
-    
+    </div><div class="form-group">
     <textarea class="form-control" id="Comments" name="Comments" rows="4"></textarea>
- 
+ </div>
   <button type="submit" class="btn btn-primary btn-block" type="submit">Submit</button>
   <button class="btn btn-default btn-block" type="reset">Reset</button>
-
+ </fieldset>
 </form>
+ </div>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
      <!-- /container -->
 	  </section>
