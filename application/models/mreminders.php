2b0f44f25c902d6c94da710b9948b66b7e71b871
hsazzad <sazzad.utm@gmail.com>
2014-11-17 00:55:35 +0800

    12:54

--- application/models/mreminders.php
+++ application/models/mreminders.php
@@ -3,22 +3,22 @@ class mreminders extends CI_Model
 {
   public function get_days($day)
   {
-<<<<<<< HEAD
+
     $day_start = date('Y-m-90');
-=======
+
     $day_start = date('Y-m-d', $day);
->>>>>>> origin/master
+
   $Mail_status="0";
    // $day_end = date('Y-m-d 23:59:59', $day);
     return $this->db->select('*')
       ->from('course')
-<<<<<<< HEAD
+
 	 //  ->where('Course_Date <', $day_start)
       ->where('Course_Date >',  $day)
-=======
+
 	   ->where('Course_Date <', $day_start)
    //   ->where('Course_Date >', $day_end)
->>>>>>> origin/master
+
       ->where('Mail_status =', $Mail_status)
       ->get()->result();
 	  
