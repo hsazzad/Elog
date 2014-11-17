ea73e670966a0237d779cbcbaaba90a446a52ec2
sharkcreep87 <shafeq.hasan@gmail.com>
2014-11-18 02:45:21 +0800

    cronjob fix

--- application/controllers/reminders.php
+++ application/controllers/reminders.php
@@ -12,35 +12,19 @@ class reminders extends CI_Controller
   }
   public function index()
   {
-    if(!$this->input->is_cli_request())
-  {
-      echo "This script shows that the cron job is running for email reminder." . PHP_EOL;
-      return;
-  }
+   
   date_default_timezone_set('Asia/Kuala_Lumpur'); 
   $timestamp = strtotime("+90 days" , date("Y-m-d H:i:s"));
  
   $reminder = $this->mreminders->get_days($timestamp);
-  if($reminder)
+  if(!empty($reminder))
   {
   foreach($reminder as $row)
   {
 
       $rid= $row->id;
-          $email= $row->Email;
-		  $name = $row->Name;
 
-         $this->emailmodel->sendRemainderEmail($email,$name);
-          $this->mreminders->mark_reminded($rid);
-		  }
-    }
-	else
-	{
-	$rid= $row->id;
-	$this->db->where('id', $rid)->update('course', array('Mail_status' => 1));
-	}
 
-      $rid=$row->id;
 	
 	  $query1 = $this->db->get_where('course', array('id' => $rid));
 foreach ($query1->result() as $row1)
@@ -59,15 +43,17 @@ echo $email;
 	  
 	 
          $this->emailmodel->sendRemainderEmail($email,$name);
-          $this->mreminders->mark_reminded($rid); }
+          $this->mreminders->mark_reminded($rid); 
+		  }
     
   } 
   }
-  else{
-  echo "no data found!!!";
   
-
+  else{
+  echo "no data";
   }
+  }
+  
   
   
   }
--- application/views/vreg.php
+++ application/views/vreg.php
@@ -40,6 +40,17 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 
   </head>
+  <style>
+
+
+
+  input {
+  text-transform: uppercase;
+}
+  
+
+
+</style>
 
   <body class="lock-screen">
 
